41727d9550876ada972df905a5d334e716b205a9
xuan.liu <xuan.liu@1012wellness.com>
2021-06-18 17:45:01 +0800

    feat: home

--- src/mock/homeConfig.js
+++ src/mock/homeConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   data: {
     bannerVos: [
       {
-        id: 1178,
+        id: 1,
         imageUrl: '/src/assets/images/home/banner1.png',
         videoUrl: null,
         videoFirstImageUrl: null,
@@ -12,7 +12,7 @@ module.exports = {
         productId: 1654,
       },
       {
-        id: 1095,
+        id: 2,
         imageUrl: '/src/assets/images/home/banner2.png',
         videoUrl: null,
         videoFirstImageUrl: null,
@@ -20,12 +20,28 @@ module.exports = {
         productId: 1618,
       },
       {
-        id: 1169,
+        id: 3,
         imageUrl: '/src/assets/images/home/banner3.png',
         videoUrl: null,
         videoFirstImageUrl: null,
         jumpUrl: '',
         productId: 1653,
+      },
+    ],
+    moduleVos: [
+      {
+        id: 1,
+        title: '喜茶百货',
+        copywriting: '果汁茶全新上市',
+        imageUrl: '/src/assets/images/home/imageUrl.png',
+        jumpUrl: '',
+      },
+      {
+        id: 2,
+        title: '阿喜团餐',
+        copywriting: '最高享9折火热预定中',
+        imageUrl: '/src/assets/images/home/drinks.png',
+        jumpUrl: '',
       },
     ],
   },
---
+++ src/mock/userInfo.js
@@ -0,0 +1,15 @@
+module.exports = {
+  code: 1,
+  message: 'SUCCESS',
+  data: {
+    usableScore: 155, // 可用积分
+    userName: '胖子也有幸福',
+    email: null,
+    phone: '15927000280',
+    userId: 33322513,
+    isVip: 0,
+    vipLevel: 'Lv1',
+    avatarUrl:
+      'https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIS0nk3C1ibMgqk5dP306bJ3bMDeeRvnEV4ickhVJiajUQhTEJ3u1l3SQHqrGG1iavSa6ePeFbU8kgzjg/132',
+  },
+};
